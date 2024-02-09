c36f7908ca7ccdaf7094790320da1845f58d189d
bernardperru <47976460+bernardperru@users.noreply.github.com>
2024-02-09 17:31:40 +0100

    Orderlist coming along...

--- src/components/OrderList/OrderItem.tsx
+++ src/components/OrderList/OrderItem.tsx
@@ -1,3 +1,9 @@
-export function OrderItem() {
-  return <div></div>;
+import { OrderListItem } from "../../Api";
+
+type Props = {
+  item: OrderListItem;
+};
+
+export function OrderItem({ item }: Props) {
+  return <div>{item.name}</div>;
 }
--- src/components/OrderList/OrderList.tsx
+++ src/components/OrderList/OrderList.tsx
@@ -1,3 +1,42 @@
+import { useEffect, useState } from "react";
+import { Api, OrderListItem } from "../../Api";
+import { OrderItem } from "./OrderItem";
+
 export function OrderList() {
-  return <div></div>;
+  const [data, setData] = useState<OrderListItem[]>();
+  const api = new Api();
+
+  useEffect(() => {
+    const getBeers = async () => {
+      const orderList = await api.getOrderListBeer();
+      setData(orderList);
+    };
+    if (!data) {
+      getBeers();
+    }
+  });
+
+  if (!data) {
+    return <div></div>;
+  }
+
+  return (
+    <div className="absolute bg-[#F5F5F5] w-3/12 h-96 left-60">
+      <span>12 produkter</span> <span>total: 400000</span>
+      {data.map((item) => (
+        <OrderItem item={item}></OrderItem>
+      ))}
+    </div>
+  );
 }
+
+// /* Rectangle 4 */
+
+// position: absolute;
+// width: 672px;
+// height: 310px;
+// left: 240px;
+// top: 269px;
+
+// background: #F5F5F5;
+// border-radius: 10px;
