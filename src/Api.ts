e6e24196f951119fa8afb948b9600b27b0af52da
bernardperru <47976460+bernardperru@users.noreply.github.com>
2024-02-09 16:49:10 +0100

    Update Api.ts

--- src/Api.ts
+++ src/Api.ts
@@ -4,11 +4,40 @@ export type BeerReponse = {
   first_brewed: string;
 };
 
+export type OrderListItem = {
+  id: number;
+  name: string;
+  first_brewed: string;
+  attenuation_level: number;
+  ph: number;
+};
+
+export type FavoriteItem = {
+  id: number;
+  name: string;
+};
+
 export class Api {
-  public async getBeer() {
+  public async getBeer(): Promise<BeerReponse[]> {
     const url = "https://api.punkapi.com/v2/beers";
     const beerRequest = await fetch(url, { method: "GET" });
     const beerResponse = (await beerRequest.json()) as BeerReponse[];
+
+    return beerResponse;
+  }
+
+  public async getOrderListBeer(): Promise<OrderListItem[]> {
+    const url = "https://api.punkapi.com/v2/beers";
+    const beerRequest = await fetch(url, { method: "GET" });
+    const beerResponse = (await beerRequest.json()) as OrderListItem[];
+
+    return beerResponse;
+  }
+
+  public async getFavorites(): Promise<FavoriteItem[]> {
+    const url = "https://api.punkapi.com/v2/beers";
+    const beerRequest = await fetch(url, { method: "GET" });
+    const beerResponse = (await beerRequest.json()) as FavoriteItem[];
 
     return beerResponse;
   }
